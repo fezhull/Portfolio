b368c74ee02726bdaf5efda4adc8203187ae52f3
pinchess68 <pinchesspinchess68@gmail.com>
2024-03-15 03:46:03 +0300

    unfinshed portfolio

---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./scrollup.css";
+
+const ScrollUp = () => {
+    
+  return (
+    <a href="#" className='scrollup'>
+        <i className='uil uil-arrow-up scrollup__icon'></i>
+    </a>
+  )
+};
+
+export default ScrollUp
